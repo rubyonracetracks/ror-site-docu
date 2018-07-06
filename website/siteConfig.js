92c0eb85af40a21b86d2cd3b9135d21164b3c01b
Jason Hsu <jhsu802701@jasonhsu.com>
2018-07-06 23:36:09 +0000

    Changed colors from green to red

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -54,8 +54,8 @@ const siteConfig = {
 
   /* colors for website */
   colors: {
-    primaryColor: '#2E8555',
-    secondaryColor: '#205C3B',
+    primaryColor: '#440000',
+    secondaryColor: '#660000',
   },
 
   /* custom fonts for website */
