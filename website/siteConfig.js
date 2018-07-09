863a37bc775a4701425542dd2b9fd6e692d14cf5
Jason Hsu <jhsu802701@jasonhsu.com>
2018-07-09 18:03:12 +0000

    Removed users section

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,18 +8,6 @@
 // See https://docusaurus.io/docs/site-config.html for all the possible
 // site configuration options.
 
-/* List of projects/orgs using your project for the users page */
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
   title: 'Ruby on Racetracks' /* title for your website */,
   tagline: "There's Ruby on Racetracks, and there's Not Exactly.  Make sure you choose the correct one!",
@@ -44,9 +32,6 @@ const siteConfig = {
     {page: 'prerequisites', label: 'Prerequisites'},
     {page: 'help', label: 'Help'}
   ],
-
-  // If you have users set above, you add it here:
-  users,
 
   /* path to images for header/footer */
   headerIcon: 'img/docusaurus.svg',
